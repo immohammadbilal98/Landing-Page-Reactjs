ef82549a1d4778f08472f67a2869e6733ae42477
immohammedbilal98 <im.mohammedbilal98@gmail.com>
2021-12-19 22:42:46 +0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+import Header from './Components/Header';
+
+function App() {
+
+  return (
+    <div className="App">
+      {        
+        <>   
+        <Header/>
+        </>
+      }      
+    </div>
+  );
+}
+
+export default App;
